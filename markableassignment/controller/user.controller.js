5dcb639d1d44b2e791e90ee53f618daf5f68ef75
Akshit Sharma <akshits349@gmail.com>
2024-05-07 23:06:03 +0530

    add the asyncwrapper middleware

--- markableassignment/controller/user.controller.js
+++ markableassignment/controller/user.controller.js
@@ -1,34 +1,22 @@
 import pool from "../db/db.js";
-
+import asyncWrapper from "../middleware/asyncWrapper.middleware.js"
 // Controller function to add a new patient
-const addPatient = async (req, res) => {
-    try {
-        const { name, gender, age, address, phone_number } = req.body;
-        
-        // Insert query
-        const query = `
-            INSERT INTO patients (name, gender, age, address, phone_number)
-            VALUES ($1, $2, $3, $4, $5)
-            RETURNING *;
-        `;
-        // console.log(query)
+const addPatient = asyncWrapper(async (req, res) => {
+    const { name, gender, age, address, phone_number } = req.body;
+    // Insert query
+    const query = `
+        INSERT INTO patients (name, gender, age, address, phone_number)
+        VALUES ($1, $2, $3, $4, $5)
+        RETURNING *;
+    `;
+    // Execute the query
+    const { rows } = await pool.query(query, [name, gender, age, address, phone_number]); 
+    res.status(201).json({
+        success: true,
+        message: "Patient added successfully",
+        data: rows[0]
+    });
+});
 
-        // Execute the query
-        const {rows} = await pool.query(query, [name, gender, age, address, phone_number]);
-        
-        res.status(201).json({
-            success: true,
-            message: "Patient added successfully",
-            data:rows[0]
-        });
-    } catch (error) {
-        console.error("Error adding patient:", error);
-        res.status(500).json({
-            success: false,
-            message: "An error occurred while adding the patient",
-            error: error.message
-        });
-    }
-};
 
 export { addPatient };
